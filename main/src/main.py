54775dbd8c3308ac7281e9ba1441c8b04c8d1dbb
Kravchuk Roman <kravchuk.roman02@gmail.com>
2022-05-16 01:11:10 +0800

    refactor: renamed constans in UPPERCASE_WITH_UNDERSCORES
    also fixed imports

--- main/src/main.py
+++ main/src/main.py
@@ -2,12 +2,14 @@ import discord
 import os
 from discord.ext import commands
 
-from config import Token, PREFIX
+from config import TOKEN, PREFIX
 
-client = commands.Bot(command_prefix=PREFIX, help_command=None, intents=discord.Intents.all())
+client = commands.Bot(command_prefix=PREFIX,
+                      help_command=None,
+                      intents=discord.Intents.all())
 
 for filename in os.listdir("src/cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-client.run(Token)
+client.run(TOKEN)
